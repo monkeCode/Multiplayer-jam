04f88f9796513dd6a0adfcea07623fdacdd6da43
Georgy <georgy.tertychniy@yandex.ru>
2022-08-04 21:12:30 +0300

    1

--- Assets/Scripts/LvlManager.cs
+++ Assets/Scripts/LvlManager.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
-public class LvlManager : MonoBehaviour
+public class LvlManager : MonoBehaviourPun
 {
     [SerializeField] private Transform _player1Pos;
     [SerializeField] private Transform _player2Pos;
     void Start()
     {
-        ResetPlayers();
+        photonView.RPC(nameof(ResetPlayers), RpcTarget.All);
     }
-
+    [PunRPC]
     public void ResetPlayers()
     {
         if(Server.Instance.Player1!= null)
